9b98414c3f1d1717a545f5cefc5a35f254d1ab40
huybinh0204 <huybinh020403@gmail.com>
2021-04-15 14:20:53 +0700

    12

--- src/Screens/QRCode/QRCodeView.js
+++ src/Screens/QRCode/QRCodeView.js
@@ -1,13 +1,26 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
-const QRCodeView =()=> {
+import {StyleSheet, Text, View} from 'react-native';
+import {RNCamera} from 'react-native-camera';
 
-        return (
-            <View>
-                <Text>adc</Text>
-            </View>
-        );
+const QRCodeView = () => {
+    const [email, setEmail] = React.useState('');
+
+    return (
+        <View style={styles.container}>
+            <Text>2</Text>
+            <RNCamera
+                ref={ref => {
+                    console.log(ref);
+                }}
+                style={{flex:1, width: '100%'}}
+            ></RNCamera>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1
     }
-
-
+});
 export default QRCodeView;
